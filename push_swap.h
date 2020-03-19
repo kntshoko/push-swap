241fbe33d2a5187cafa41defe2e193b947c6ed8a
root <root@localhost.localdomain>
2020-03-19 10:06:05 +0200

    push_swap

--- push_swap.h
+++ push_swap.h
@@ -14,18 +14,20 @@
 # define PUSH_SWAP_H
 
 # include "libft/libft.h"
-# include <stdio.h>
 
-int     mx(int *arr, int len);
-int     mn(int *arr, int len);
-int     sort(int *arr, int len);
-int    target(int n, int *arr, int len);
-void    s(int *a, int len);
-void    p(int *a, int *alen, int *b, int *blen);
-void    r(int *a, int i);
-void    rr(int *a, int i);
-int     rev(int n, int *arr, int len);
-int     rot(int n, int *arr, int len);
-void    place(int n, int *arr, int len);
-int		md(int *arr, int len);
+void	s(int *a, int len);
+void	p(int *a, int *alen, int *b, int *blen);
+void	r(int *a, int i);
+void	rr(int *a, int i);
+void	place(int n, int *arr, int len);
+int	rev(int n, int *arr, int len);
+int	rot(int n, int *arr, int len);
+int	md(int *arr, int len);
+int	mx(int *arr, int len);
+int	mn(int *arr, int len);
+int	sort(int *arr, int len);
+int	target(int n, int *arr, int len);
+int	*convert(char **str, int len);
+int	wordcount(char **str);
+
 #endif
