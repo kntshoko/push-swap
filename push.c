04e624fac591e5ba1e7120a3af469f5e446ad0a8
kntshoko <50703727+kntshoko@users.noreply.github.com>
2020-05-11 18:36:06 +0200

    Update push.c
    clean up

--- push.c
+++ push.c
@@ -41,20 +41,6 @@ static int	mdb(int *arr, int len)
 	return (temp);
 }
 
-int mid(int m, int *a, int alen)
-{
-	int i;
-
-	i = 0;
-	while (i < alen)
-	{
-		if (a[i] < m)
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
 int midb(int m, int *b, int blen)
 {
 	int i;
