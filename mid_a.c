00f9f2c9235cfb3bb9d0514325fc82465331e99b
kntshoko <50703727+kntshoko@users.noreply.github.com>
2020-05-11 18:57:37 +0200

    Update mid_a.c
    clean up

--- mid_a.c
+++ mid_a.c
@@ -12,3 +12,31 @@ int mid(int m, int *a, int alen)
 	}
 	return (1);
 }
+
+int	md(int *arr, int len)
+{
+	int *a;
+	int temp;
+	int i;
+	int j;
+
+	a = malloc(len * sizeof(int));
+	ft_memcpy(a,arr,len*sizeof(int));
+	i = -1;
+	while(++i < len)
+	{
+		j = -1;
+		while(++j < len)
+		{
+			if (a[i] <  a[j] )
+			{
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+	temp = a[len /2];
+	free(a);
+	return (temp);
+}
