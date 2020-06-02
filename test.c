412c6eb1c999df38345c98ff06ca8db5b9680921
kntshoko <50703727+kntshoko@users.noreply.github.com>
2020-06-02 20:21:49 +0630

    Delete test.c

--- test.c
+++
@@ -1,8 +0,0 @@
-#include "push_swap.h"
-
-int main()
-{
-	int a[6] = {7,6,0,5,1,3};
-	printf("r = %d, %d\n", to_r(3,a,6), rot(to_r(3,a,6),a,6));
-	printf("rr = %d, %d\n", to_rr(3,a,6), rev(to_rr(3,a,6),a,6));
-}
